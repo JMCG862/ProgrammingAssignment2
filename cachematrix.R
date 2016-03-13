ae9c330f62db724a76f70ec1ed44c50ab38496b8
JMCG862 <jeffrey_cottle@hotmail.com>
2016-03-12 23:14:35 -0500

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function has two functions, setmatrix and getmatrix. 
+## setmatrix is used to set the value of the matrix in the global environment
+## getmatrix returns the values of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix.inverse<- NULL
+setmatrix <- function(y){ 
+ matrix.inverse <<- NULL
+ x<<-y
+}
+getmatrix <- function()x
+list(getmatrix = getmatrix, setmatrix = setmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This functions searches for the value of m, 
+## if there is no value for m, then it calculates it with the solve() and stores it in m trough the MakeCacheMatrix function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setmatrix(m) m
+
